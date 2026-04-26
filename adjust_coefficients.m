3718ea56e79a424e24a038ce8731d1272ea2ee31
Cecilia103 <chenyx728@mail2.sysu.edu.cn>
2026-04-26 21:33:54 +0800

    Add files via upload

--- adjust_coefficients.m
+++ adjust_coefficients.m
@@ -1,9 +1,9 @@
-% 你只缺这个论文函数，直接复制
 function I_coef = adjust_coefficients(O, S, qt_o, qt_c)
     [h, w] = size(O);
     I_coef = O;
     for i = 1:h
         for j = 1:w
+	    % 定位 8×8 块内位置，获取当前系数的量化步长
             mo = qt_o(mod(i-1,8)+1, mod(j-1,8)+1);
             mc = qt_c(mod(i-1,8)+1, mod(j-1,8)+1);
             z = O(i,j) * mo / mc;
