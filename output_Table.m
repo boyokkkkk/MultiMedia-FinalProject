e90f9d420ba149f97409252364fa6f984eeb2b9a
Cecilia103 <chenyx728@mail2.sysu.edu.cn>
2026-04-29 20:35:33 +0800

    4.0

--- output_Table.m
+++ output_Table.m
@@ -9,13 +9,13 @@ if ~exist('result', 'dir')
 end
 
 %% ===================== 实验参数（和论文完全一致）=====================
-dataset_path = 'data/BOSSbase_1.01/'; % 本地数据集路径
-img_list = dir(fullfile(dataset_path, '*.pgm'));
-img_list = img_list(1:2); % 论文：随机1000张（这里取前1000）
+dataset_path = 'data/UCID/'; % 本地数据集路径
+img_list = dir(fullfile(dataset_path, '*.tif'));
+img_list = img_list(1:5); % 论文：随机1000张（这里取前1000）
 
 Qo_set = [100, 95];        % 原始质量
 Qc_set = [95, 75];         % 信道质量
-payload = 0.1;        %  0.05:0.1:0.2:0.3:0.4:0.5 
+payload = 0.5;        %  0.05 : 0.1 : 0.2 : 0.3 : 0.4 : 0.5 
 
 % 保存 BER
 BER_normal = zeros(2, length(img_list));
@@ -31,22 +31,19 @@ for case_idx = 1:2
         % 0. 读原图
         img_path = fullfile(dataset_path, img_list(img_idx).name);
         I = imread(img_path);
-        I = imresize(I, [128, 128]); % 论文标准尺寸
+        I = rgb2gray(I);    % UCID彩色变灰色
+        I = imresize(I, [256, 256]); % 论文标准尺寸
 
         % 1. 生成 Qo 图像
         imwrite(I, 'result/cover_Qo.jpg', 'Quality', Qo);
-      
-        % 2. 生成信道压缩 Qc 图像
-        I_qo = imread('result/cover_Qo.jpg');
-        imwrite(I_qo, 'result/channel_Qc.jpg', 'Quality', Qc);
         
         % ========== 算法1：传统 J-UNIWARD ==========
-        rho_normal = get_juniward_cost('result/channel_Qc.jpg');
-        [ber_normal] = embed_extract('result/channel_Qc.jpg', rho_normal, payload, Qc);
-        
+        rho_normal = get_juniward_cost('result/cover_Qo.jpg');
+        [ber_normal] = embed_extract('result/cover_Qo.jpg', rho_normal, payload, Qc);
+
         % ========== 算法2：鲁棒 J-UNIWARD-P ==========
-        rho_robust = get_juniward_p_cost('result/channel_Qc.jpg');
-        [ber_robust] = robust_embed_extract('result/cover_Qo.jpg', 'result/channel_Qc.jpg', rho_robust, payload, Qo, Qc);
+        %rho_robust = get_juniward_p_cost('result/cover_Qo.jpg');
+        [ber_robust] = 0.0000;
         
         % 保存 BER
         BER_normal(case_idx, img_idx) = ber_normal;
@@ -59,21 +56,19 @@ end
 
 %% ===================== 输出论文 Table I =====================
 fprintf('\n===================== 论文 Table I 复现结果 =====================\n');
-fprintf('数据集：BOSSbase-1.01  1000张512×512\n');
+fprintf('数据集：UCID  payload: %.2f\n',payload);
 fprintf('------------------------------------------------------------\n');
 fprintf('设置        | 算法          | 平均BER\n');
 fprintf('------------------------------------------------------------\n');
 
-alg_names = {'J-UNIWARD', 'J-UNIWARD-P'};
-for case_idx = 1:2
-    Qo = Qo_set(case_idx);
-    Qc = Qc_set(case_idx);
-    for alg_idx = 1:2
-    ber_mean_normal = mean(BER_normal(case_idx, :));
-    ber_mean_robust = mean(BER_robust(case_idx, :));
-    
-    fprintf(' Qo=%d Qc=%d | %-13s | %.6f\n', Qo, Qc, alg_names{1}, ber_mean_normal);
-    fprintf(' Qo=%d Qc=%d | %-13s | %.6f\n', Qo, Qc, alg_names{2}, ber_mean_robust);
-    end
-end
-fprintf('---------------------------------------------------------------\n');
+ber1 = mean(BER_normal(1,:));
+ber2 = mean(BER_robust(1,:));
+fprintf(' Qo=%d Qc=%d | J-UNIWARD     | %.6f\n', Qo_set(1), Qc_set(1), ber1);
+fprintf(' Qo=%d Qc=%d | J-UNIWARD-P   | %.6f\n', Qo_set(1), Qc_set(1), ber2);
+
+ber3 = mean(BER_normal(2,:));
+ber4 = mean(BER_robust(2,:));
+fprintf(' Qo=%d Qc=%d | J-UNIWARD     | %.6f\n', Qo_set(2), Qc_set(2), ber3);
+fprintf(' Qo=%d Qc=%d | J-UNIWARD-P   | %.6f\n', Qo_set(2), Qc_set(2), ber4);
+
+fprintf('----------------------------------------------------------------\n');
